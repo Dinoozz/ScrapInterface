f1c7bdbe720e7f9e48d23199ed72a8b9033424a7
CamilleLecoq <55230689+EstebanEpitech@users.noreply.github.com>
2023-12-06 11:27:18 +0100

    [+]

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://api2.camille-lecoq.com:5000/api';
+const API_BASE_URL = 'https://api2.camille-lecoq.com/api';
 
 const getAuthConfig = () => {
     const JWtoken = localStorage.getItem('JWToken');
